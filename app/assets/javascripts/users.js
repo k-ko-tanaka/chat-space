99caf2adc6865f749fce7c76b2eb4fb96a19efd3
k-ko-tanaka <59305203+k-ko-tanaka@users.noreply.github.com>
2020-02-16 18:52:00 +0900

    add_fix_view1

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,26 +1,26 @@
 $(function(){
   function addUser(user) {
     let html = `
-      <div class="chat-group-form__search clearfix">
-        <p class="chat-group-user__input">${user.name}</p>
+      <div class="chat-group-user clearfix">
+        <p class="chat-group-user__name">${user.name}</p>
         <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
       </div>
-    `;
-    $("#user-search-result").append(html);
-  }
+　　`;
+　　$("#user-search-result").append(html);
+　　}
 
   function addNoUser() {
     let html = `
-      <div class="chat-group-form__search clearfix">
-        <p class="chat-group-user__input">ユーザーが見つかりません</p>
+      <div class="chat-group-user clearfix">
+        <p class="chat-group-user__name">ユーザーが見つかりません</p>
       </div>
     `;
     $("#user-search-result").append(html);
   }
   function addDeleteUser(name, id) {
     let html = `
-    <div class="chat-group-form__search clearfix" id="${id}">
-      <p class="chat-group-user__input">${name}</p>
+    <div class="chat-group-user clearfix" id="${id}">
+      <p class="chat-group-user__name">${name}</p>
       <div class="user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn" data-user-id="${id}" data-user-name="${name}">削除</div>
     </div>`;
     $(".js-add-user").append(html);
